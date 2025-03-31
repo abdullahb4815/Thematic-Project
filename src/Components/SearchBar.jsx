b2229030ee24d7262b4122d983ff28949d8cf47d
yussuf <yussuf@DESKTOP-HS0LICC>
2025-03-31 23:45:42 +0100

    Added the table view and get more button
    needs more styling but is functional and works gets more data and works with the sorting by name/genre functionality I added

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -8,10 +8,8 @@ export default function SearchBar  (/*{ data, onSearch}*/)  {
 
     const handleSearch = (event) => { 
         const value = event.target.value;
+        setOffSet(0);
         setSearchTerm(value);
-
-
-
 
     // commented out as this is used to handle preloadedd data not when dynamically loading data
     // const filteredResults = data.filter(item =>
@@ -21,17 +19,23 @@ export default function SearchBar  (/*{ data, onSearch}*/)  {
     // onSearch(filteredResults); 
     };
 
+    const handleGetMore = () => {
+        setOffSet(prevOffset => prevOffset + 20);
+        
+    }
+
 
     const [searchedApps, setSearchedApps] = useState([]);
-
+    const [offSetInt, setOffSet] = useState(0);
+ 
     useEffect(() => {
         const fetchData = async () => {
-        const apps = await searchApps(searchTerm);
+        const apps = await searchApps(searchTerm, offSetInt);
         console.log(apps);
         setSearchedApps(apps);
         };
         fetchData();
-    }, [searchTerm]);
+    }, [searchTerm, offSetInt]);
 
     
 
@@ -56,15 +60,52 @@ export default function SearchBar  (/*{ data, onSearch}*/)  {
             </div>
 
             <div className='results-container'>
-                <ul>
+                {/* <ul>
                     {searchedApps.map(app => (
                     <li key={app.app_id}>
                         <strong>ID:</strong> {app.app_id} - <strong>Name:</strong> {app.app_name} - <strong>Genre:</strong> {app.genre_table?.genre_type}
                     </li>
                     ))}
-                </ul>
+                </ul> */}
+
+
+                <table>
+                    <thead>
+                        <tr className='row_header'>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Genre</th>
+                            <th>Rating</th>
+                            <th>Download Count</th>
+                            <th>Price</th>
+                            <th>Currency</th>
+                        </tr>
+                    </thead>
+
+                    <tbody className='row_data'>
+                        {searchedApps.map(app => (
+                            <tr className='row_data' key={app.app_id}>
+                                <td>{app.app_id}</td>
+                                <td>{app.app_name}</td>
+                                <td>{app.genre_table?.genre_type}</td>
+                                <td>{app.user_rating}</td>
+                                <td>{app.download_count}</td>
+                                <td>{app.price}</td>
+                                <td>{app.currency_table?.currency_type}</td>
+                            </tr>
+                            
+                        ))}
+                    </tbody>
+                </table>
+
+                <button id='getMoreButton' onClick={handleGetMore}> See more -{">"} </button>
+
+                
+
 
             </div>
+
+            
         </div>
     );
 };
