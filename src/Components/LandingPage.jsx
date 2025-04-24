fe7c04ff4f433af54a51661240712660b7ac8fbf
dj-khaled1 <23660034@stu.mmu.ac.uk>
2025-04-24 22:35:54 +0100

    Changed landing page image and added some css.

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import './LandingPage.css'; // Import the CSS
+import React from "react";
+import "./LandingPage.css";
 
-const LandingPage = () => {
+export default function LandingPage() {
+
   return (
-    <div class="box">
-      <h1 class="title">
-        <div>
-          <span>App Base</span>
-        </div>
-      </h1>
-      <h2>The best engine for app searching on the market </h2>
-      <p>App Base is a unique, up-to-date search engine designed specifically for discovering apps in the iOS App Store. Unlike the default store search, App Base makes it easier to find trending, new, and niche apps quickly with smarter filters and real-time updates. It helps users explore beyond the top charts, making app discovery more efficient and personalized.</p>
-</div>
-
+    <div id="landing-page">
+      <div id="landing-text">
+        <h1 id="title">App Base</h1>
+        <h2 id="title-tag">The best engine for app searching on the market </h2>
+        <h3 id="description">
+          App Base is a unique, up-to-date search engine designed specifically for discovering apps in the iOS App Store.
+          Unlike the default store search, App Base makes it easier to find trending, new, and niche apps quickly with smarter filters and real-time updates.
+          It helps users explore beyond the top charts, making app discovery more efficient and personalized.
+        </h3>
+      </div>
+    </div>
   );
-};
+}
 
-export default LandingPage;
